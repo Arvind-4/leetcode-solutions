34f441a24779fabc153f1a3953e02c254e8a47f4
Arvind-4 <lordarvindthegamer@gmail.com>
2023-05-20 23:59:23 +0530

    Added: Code for 20-05-2023

---
+++ course-schedule/Solution.py
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/course-schedule
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dic = {
+            i:[] for i in range(numCourses)
+        }
+        for crs, pre in prerequisites:
+            dic[crs].append(pre)
+        visit = set()
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if dic[crs]==[]:
+                return True
+            visit.add(crs)
+            for pre in dic[crs]:
+                if not dfs(pre):
+                    return False
+            visit.remove(crs)
+            dic[crs] = []
+            return True
+        for crs in range(numCourses):
+            if not dfs(crs):return False
+        return True
---
+++ reverse-bits/Solution.py
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/reverse-bits
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return int((('{0:032b}'.format(n))[::-1]),2)
