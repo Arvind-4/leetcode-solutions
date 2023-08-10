403175addca3d6eb9edd0e06e543ad9fc5e16ab4
Arvind-4 <lordarvindthegamer@gmail.com>
2023-08-10 23:16:44 +0530

    Updated: delete.py file

--- delete.py
+++ delete.py
@@ -4,4 +4,7 @@ for root, dirs, files in os.walk("."):
     for dir in dirs:
         if dir in dirNames:
             shutil.rmtree(os.path.join(root, dir))
-            
+            try:
+                os.remove(os.path.join(root, "Solution.Identifier"))
+            except:
+                pass
